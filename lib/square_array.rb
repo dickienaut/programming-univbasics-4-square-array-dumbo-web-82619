c9855cff9a0dfb0153298af468a740b5ab0a913b
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-13 21:41:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   result = []
-  while counter < array length do
+  while counter < array.length do
     result.push(array[counter] * array[counter])
     counter += 1
   end
